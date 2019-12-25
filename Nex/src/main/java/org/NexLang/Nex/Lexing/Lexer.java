356eb00e0047e53acf4317f749559c18f204dec4
Daniel Munch <daniellmunch@gmail.com>
2019-12-25 15:04:29 +0100

    Did some more

--- Nex/src/main/java/org/NexLang/Nex/Lexing/Lexer.java
+++ Nex/src/main/java/org/NexLang/Nex/Lexing/Lexer.java
@@ -7,7 +7,32 @@ import java.util.List;
 public class Lexer
 {
 
-    private static String Read;
-    private static List<Token> Tokens;
+    private String Read;
+    private List<Token> Tokens;
+    private static int Index = 0;
+
+    public Lexer (String Read, List<Token> Tokens)
+    {
+        this.Read = Read;
+        this.Tokens = Tokens;
+    }
+
+    public void Lex ()
+    {
+
+    }
+
+    private char Peek ()
+    {
+        return Read.charAt (Index + 1);
+    }
+
+    private char Peek (int Amount)
+    {
+        return Read.charAt (Index + Amount);
+    }
+
+    public String GetRead () { return Read; }
+    public List<Token> GetTokens () { return Tokens; }
 
 }
--- Nex/src/main/java/org/NexLang/Nex/Tokens/Location.java
+++ Nex/src/main/java/org/NexLang/Nex/Tokens/Location.java
@@ -1,4 +1,29 @@
 package org.NexLang.Nex.Tokens;
 
-public class Location {
+public class Location
+{
+
+    private int StartLine;
+    private int EndLine;
+    private int StartColumn;
+    private int EndColumn;
+
+    public Location (int StartLine, int EndLine, int StartColumn, int EndColumn)
+    {
+        this.StartLine = StartLine;
+        this.EndLine = EndLine;
+        this.StartColumn = StartColumn;
+        this.EndColumn = EndColumn;
+    }
+
+    public Location None ()
+    {
+        return new Location (0, 0, 0, 0);
+    }
+
+    public Location Copy ()
+    {
+        return new Location (this.StartLine, this.EndLine, this.StartColumn, this.EndColumn);
+    }
+
 }
